a516f6d3bfdba611b9bc31fb55a588bf527bbd45
harshvardhan das <dasharshvardhan0@gmail.com>
2026-05-06 14:55:12 +0530

    fix correct App import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { LanguageProvider } from './context/LanguageContext'
-import App from './App' 
+import app from './app'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
