a9a455f520b74887d36377edee8f0d7debef0cca
harshvardhan das <dasharshvardhan0@gmail.com>
2026-05-06 14:52:15 +0530

    fix correct App import

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -171,6 +171,7 @@ function Navbar() {
 }
 
 export default function App() {
+  return <h1 style={{color:"white"}}>WORKING 🚀</h1>
   // App open hote hi visit track karo
   useEffect(() => {
     trackVisit()
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { LanguageProvider } from './context/LanguageContext'
-import app from './app'
+import App from './App' 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
