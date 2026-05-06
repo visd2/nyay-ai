0349fd3bc74aafca98f4abf62eda5230766e5610
harshvardhan das <dasharshvardhan0@gmail.com>
2026-05-06 13:57:00 +0530

    added App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { LanguageProvider } from './context/LanguageContext.jsx'
-import App from "./components/App.jsx";
+import App from "./App.jsx";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
