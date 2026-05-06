282987679b6d792f00cc6533d73723f5ac858858
harshvardhan das <dasharshvardhan0@gmail.com>
2026-05-06 09:55:15 +0530

    Fix: Add .jsx extension in main.jsx imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import { LanguageProvider } from './context/LanguageContext'
-import App from './App'
+import { LanguageProvider } from './context/LanguageContext.jsx'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
